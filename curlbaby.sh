fc1350e78ee86c1ae18a9e05457ce82521a88234
Sushil Pandey <contact.sushilpandey@gmail.com>
2025-03-26 12:05:09 +0530

    Structured

--- curlbaby.sh
+++ curlbaby.sh
@@ -1,12 +1,8 @@
- 
+#!/bin/bash
 mkdir -p curlbaby/target/classes
- 
-echo "Compiling..."
-javac -d curlbaby/target/classes curlbaby/src/main/java/com/curlbaby/CurlBabyApp.java
- 
+javac -cp "curlbaby/lib/*" -d curlbaby/target/classes curlbaby/src/main/java/com/curlbaby/CurlBabyApp.java
 if [ $? -eq 0 ]; then
-  echo "Compilation successful. Starting Curl Baby..."
-  java -cp "curlbaby/target/classes" com.curlbaby.CurlBabyApp "$@"
+  java -cp "curlbaby/target/classes:curlbaby/lib/*" com.curlbaby.CurlBabyApp "$@"
 else
   echo "Compilation failed. Please fix the errors."
 fi
